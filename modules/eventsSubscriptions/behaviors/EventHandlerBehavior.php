0e20d094a2a0e15a9e4dbc6b955c78bedee837fd
Rodion Abaturov <ab.rodion@gmail.com>
2023-06-27 16:58:26 +0700

    Fix phpDoc

--- modules/eventsSubscriptions/behaviors/EventHandlerBehavior.php
+++ modules/eventsSubscriptions/behaviors/EventHandlerBehavior.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace app\modules\eventsSubscriptions\behaviors;
 
 use app\modules\eventsSubscriptions\models\EventSubscription;
+use app\modules\eventsSubscriptions\models\IWorker;
 use yii\base\Behavior;
 use yii\base\Event;
 use yii\helpers\ArrayHelper;
@@ -13,10 +14,10 @@ use yii\helpers\ArrayHelper;
  */
 class EventHandlerBehavior extends Behavior
 {
-    /** @var callable[] */
+    /** @var string[] Class names of workers that implements IWorker */
     public $workers;
 
-    /** @var string */
+    /** @var string Name of event */
     public $event;
 
     public function events(): array
@@ -36,8 +37,10 @@ class EventHandlerBehavior extends Behavior
             return;
         }
         $emails = ArrayHelper::getColumn($subscribers, 'user_email');
-        foreach ($this->workers as $worker) {
-            \call_user_func([$worker, 'doJob'], $emails, $event);
+
+        foreach ($this->workers as $workerClass) {
+            /** @see IWorker */
+            \call_user_func([$workerClass, 'doJob'], $emails, $event);
         }
     }
 }
